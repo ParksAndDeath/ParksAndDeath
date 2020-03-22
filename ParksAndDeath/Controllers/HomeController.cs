c501d3f16fda2fe4c8f1fb32d361274d2427e2ff
abraham <abehamdan34@gmail.com>
2020-03-22 17:18:01 -0400

    Added redirect to different views in the home controller based on database info

--- ParksAndDeath/Controllers/HomeController.cs
+++ ParksAndDeath/Controllers/HomeController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ParksAndDeath.Models;
@@ -11,15 +13,47 @@ namespace ParksAndDeath.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ParksAndDeathDbContext _context;
+        private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SignInManager<IdentityUser> signInManager, ParksAndDeathDbContext context)
         {
+            _context = context;
+            _signInManager = signInManager;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            //checking if user is logged in
+            if (_signInManager.IsSignedIn(User))
+            {
+                //identify the user that is logged in
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+                //checks if the user has their information filled in and if they do, it sends them to the parks list to fill their bucket list
+                try
+                {
+                    var userBl = _context.UserParks.Where(b => b.CurrentUserId == userId).First();
+                    var userInfo = _context.UserInfo.Where(x => x.OwnerId == userId).First();
+                    if (userBl != null && userInfo != null)
+                    {
+                        return RedirectToAction("UpdateUserInfo", "User");
+                    }
+                    if (userInfo != null)
+                    {
+                        return RedirectToAction("Index", "ParksDb");
+
+                    }
+                }
+                //if they do not have any info in then they get sent to the page to populate their information
+                catch
+                {
+                    return RedirectToAction("AddUserInput", "User");
+                }
+
+            }
             return View();
         }
 
