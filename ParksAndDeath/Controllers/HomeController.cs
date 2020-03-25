eb71d061cb066da858fcf7c87d1c94577c349246
abraham <abehamdan34@gmail.com>
2020-03-24 23:46:30 -0400

    merge Abe and Heathers work and pushing it up

--- ParksAndDeath/Controllers/HomeController.cs
+++ ParksAndDeath/Controllers/HomeController.cs
@@ -16,12 +16,14 @@ namespace ParksAndDeath.Controllers
         private readonly ParksAndDeathDbContext _context;
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger, SignInManager<IdentityUser> signInManager, ParksAndDeathDbContext context)
+        public HomeController(ILogger<HomeController> logger, SignInManager<IdentityUser> signInManager, ParksAndDeathDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
             _signInManager = signInManager;
             _logger = logger;
+            _userManager = userManager;
         }
 
         public IActionResult Index()
@@ -33,28 +35,47 @@ namespace ParksAndDeath.Controllers
                 var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
                 //checks if the user has their information filled in and if they do, it sends them to the parks list to fill their bucket list
-                try
-                {
-                    var userBl = _context.UserParks.Where(b => b.CurrentUserId == userId).First();
-                    var userInfo = _context.UserInfo.Where(x => x.OwnerId == userId).First();
-                    if (userBl != null && userInfo != null)
-                    {
-                        return View("FullUser"); //buttons to see parks, button to see BucketList, update user info button. parks youve visited button
-                    }
-                    if (userInfo != null)
-                    {
-                        return RedirectToAction("Index", "ParksDb");
 
-                    }
-                }
-                //if they do not have any info in then they get sent to the page to populate their information
-                catch
-                {
-                    return RedirectToAction("AddUserInput", "User");
-                }
+                var userBl = CheckUserInfo(_context, userId);
+                var userInfo = CheckBucketList(_context, userId);
 
+                if (userBl != null && userInfo != null)
+                {
+                    return View("FullUser"); //buttons to see parks, button to see BucketList, update user info button. parks youve visited button
+                }
+                if (userInfo != null)
+                {
+                    return RedirectToAction("Index", "ParksDb");
+
+                }
+                return RedirectToAction("AddUserInput", "User");
             }
             return View();
+        }
+
+        public static bool CheckUserInfo(ParksAndDeathDbContext context, string id)
+        {
+            try
+            {
+                context.UserInfo.Where(x => x.OwnerId == id).First();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static bool CheckBucketList(ParksAndDeathDbContext context, string id)
+        {
+            try
+            {
+                context.UserParks.Where(x => x.CurrentUserId == id).First();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         public IActionResult Privacy()
@@ -69,3 +90,4 @@ namespace ParksAndDeath.Controllers
         }
     }
 }
+
---
+++ ParksAndDeath/Models/Helper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace ParksAndDeath.Models
+{
+    public class Helper
+    {
+        public static bool CheckForUserInfo(UserManager<IdentityUser> userManager, ClaimsPrincipal user, ParksAndDeathDbContext context)
+        {
+            string id = userManager.GetUserId(user);
+            try
+            {
+                var found = context.UserInfo.Where(x => x.OwnerId == id).First();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
